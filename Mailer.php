d3e62fa0e99743f71c4bde4928ce783f03e846f6
Francisco Fernández Nogueira <francisco.fernandez.nogueira@gmail.com>
2013-01-21 20:43:36 +0100

    mailer component with send plain method

---
+++ Mailer.php
@@ -0,0 +1,23 @@
+<?php
+class Mailer extends CApplicationComponent
+{
+	public $backend;
+	public $params;
+	
+	public function init()
+	{
+		include('Mail.php');
+		
+		parent::init();
+	}
+	
+	public function send($from, $to, $subject, $body)
+	{
+		$headers['From'] = $from;
+		$headers['To'] = $to;
+		$headers['Subject'] = $subject;
+				
+		$mail_object =& Mail::factory($this->backend, $this->params);
+		return $mail_object->send($to, $headers, $body);
+	}
+}
