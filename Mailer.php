fae510a54efbec6de894a13c2fd3fa1116228252
Francisco Fernández Nogueira <francisco.fernandez.nogueira@gmail.com>
2013-01-21 21:23:01 +0100

    send HTML method

--- Mailer.php
+++ Mailer.php
@@ -1,15 +1,12 @@
 <?php
+include('Mail.php');
+include('Mail/mime.php');
+
 class Mailer extends CApplicationComponent
 {
 	public $backend;
-	public $params;
-	
-	public function init()
-	{
-		include('Mail.php');
-		
-		parent::init();
-	}
+	public $mailParams=array();
+	public $mimeParams=array();
 	
 	public function send($from, $to, $subject, $body)
 	{
@@ -17,7 +14,24 @@ class Mailer extends CApplicationComponent
 		$headers['To'] = $to;
 		$headers['Subject'] = $subject;
 				
-		$mail_object =& Mail::factory($this->backend, $this->params);
+		$mail_object =& Mail::factory($this->backend, $this->mailParams);
+		return $mail_object->send($to, $headers, $body);
+	}
+	
+	public function sendHTML($from, $to, $subject, $html)
+	{
+		$headers['From'] = $from;
+		$headers['To'] = $to;
+		$headers['Subject'] = $subject;
+		
+		$mime = new Mail_mime($this->mimeParams);
+		
+		$mime->setHTMLBody($html);
+		
+		$body = $mime->get();
+		$headers = $mime->headers($headers);
+		
+		$mail_object =& Mail::factory($this->backend, $this->mailParams);
 		return $mail_object->send($to, $headers, $body);
 	}
 }
